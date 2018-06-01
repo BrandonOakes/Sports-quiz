82f0b20e8f4f5e5feba51ac0ad5cb1bb86db126d
Brandon Oakes <boltfever@Brandons-MBP.home>
2018-06-01 19:25:02 -0400

    added form

--- sports_quiz.js
+++ sports_quiz.js
@@ -1,35 +1,28 @@
 var correct = 0;
-var question1 = prompt("What team won the superbowl in 2018");
-var question2 = prompt("What team the superbowl in 2017");
-var question3 = prompt("What team the NBA championship in 2017?")
-var question4 = prompt("What team the MLB World series in 2017?")
-var question5 = prompt("What team won the World series in 2016?")
-
-if (question1.toUpperCase() === "EAGLES") {
-   correct += 1;
-}
-
-if (question2.toUpperCase() === "PATRIOTS") {
-   correct += 1;
-}
-
-if (question3.toUpperCase() === "WARRIORS") {
-   correct += 1;
-}
-
-if (question4.toUpperCase() === "INDIANS") {
-   correct += 1;
-}
-
-if (question5.toUpperCase() === "CUBS") {
-   correct += 1;
+// var question = [
+//             [prompt("What team won the superbowl in 2018"), 'EAGLES'],
+//             [prompt("What team won the superbowl in 2017"), 'PATRIOTS'],
+//             [prompt("What team the NBA title in 2017?"), 'WARRIORS'],
+//             [prompt("What team won the World series in 2017?"), 'ASTROS'],
+//             [prompt("What team won the World series in 2016?"), 'CUBS']
+//           ];
+let lis = document.getElementsbyTagName('li')
+lis.addEventListener('mouseover', () => {
+  lis.setAttribute('style', 'color:red')
+})
+for (asked = 0; asked < question.length; asked +=1){
+    answer = question[asked][0];
+    if (answer.toUpperCase() === question[answer][1]){
+      correct += 1;
+    }
 }
 
 
 if (correct <= 2) {
-  document.write("<p>You got " + correct + "  questions correct, you earn the BENCH PLAYER sports trivia grade</p>");
+  var outputDiv = document.getElementById('output')
+  outputDiv.innerHTML = "<p>You got " + correct + "  questions correct, you earn the BENCH PLAYER sports trivia grade</p>";
 }else if (correct >3 && correct <5) {
-    document.write("<p>You got " + correct + " questions correct, you earn the STARTER sports trivia grade</p>");
+    outputDiv.innerHTML = "<p>You got " + correct + " questions correct, you earn the STARTER sports trivia grade</p>";
 }else if (correct === 5) {
-    document.write("<p>You got " + correct + " questions correct, you earn the ALL-STAR sports trivia grade</p>" );
+    outputDiv.innerHTML = "<p>You got " + correct + " questions correct, you earn the ALL-STAR sports trivia grade</p>";
 }

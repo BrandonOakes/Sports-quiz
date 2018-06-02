0ae6d893681ba62e46edcb9204450a760b1a43ae
Brandon Oakes <boltfever@Brandons-MBP.home>
2018-06-02 10:05:42 -0400

    README cleaned up, radio buttons and css added

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-Sports-quiz
+# Sports-quiz
 
 Sports quiz utilizing Javascript if/else if statements and prompt to ask user 5 questions related to sports. 
 If user answer between 0-2 correct they will be prompted with "Bench player". If they answer 3-4 questions 
-correctly they will be prompted with "Starter". If they answer all 5 questions correctly they will be prompted 
-with "All-Star". The questions are not case sensative.
+correctly they will be prompted with "Starter". If all 5 questions are answered correctly user will be prompted with "All-Star". The questions are not case sensative.
 
-Getting Started:
+## Getting Started:
 
-Save sports_quiz.js and index html in the same directory and open index.html in browser of choice. Refresh the
-page to answer the questions again.
+Save sports_quiz.js and index html in the same directory and open index.html in browser of choice. Refresh the page to answer the questions again.
 
 
--- sports_quiz.js
+++ sports_quiz.js
@@ -1,4 +1,4 @@
-var correct = 0;
+// var correct = 0;
 // var question = [
 //             [prompt("What team won the superbowl in 2018"), 'EAGLES'],
 //             [prompt("What team won the superbowl in 2017"), 'PATRIOTS'],
@@ -10,19 +10,3 @@ let lis = document.getElementsbyTagName('li')
 lis.addEventListener('mouseover', () => {
   lis.setAttribute('style', 'color:red')
 })
-for (asked = 0; asked < question.length; asked +=1){
-    answer = question[asked][0];
-    if (answer.toUpperCase() === question[answer][1]){
-      correct += 1;
-    }
-}
-
-
-if (correct <= 2) {
-  var outputDiv = document.getElementById('output')
-  outputDiv.innerHTML = "<p>You got " + correct + "  questions correct, you earn the BENCH PLAYER sports trivia grade</p>";
-}else if (correct >3 && correct <5) {
-    outputDiv.innerHTML = "<p>You got " + correct + " questions correct, you earn the STARTER sports trivia grade</p>";
-}else if (correct === 5) {
-    outputDiv.innerHTML = "<p>You got " + correct + " questions correct, you earn the ALL-STAR sports trivia grade</p>";
-}
